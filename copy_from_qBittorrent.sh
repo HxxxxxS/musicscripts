444410df96dba97605c7c51e6f8403e51ab8bdf5
sophusweidemann <sophusaugustweidemann@gmail.com>
2018-01-17 03:28:48 +0100

    changed everything to camelcase

--- copy_from_qBittorrent.sh
+++ copy_from_qBittorrent.sh
@@ -2,24 +2,24 @@
 
 importFolder="~/Music/Import"
 
-logfile="logs/$(date +%Y-%m)_downloads.csv"
+logFile="logs/$(date +%Y-%m)_downloads.csv"
 
-                ## Parameters for qBittorrent
-name="$1"       ## "%N"
-category="$2"   ## "%L"
-cpath="$3"      ## "%F"
-rpath="$4"      ## "%R"
-spath="$5"      ## "%D"
-files="$6"      ## "%C"
-size="$7"       ## "%Z"
-tracker="$8"    ## "%T"
-tHash="$9"      ## "%I"
+                    ## Parameters for qBittorrent
+name="$1"           ## "%N"
+category="$2"       ## "%L"
+contentPath="$3"    ## "%F"
+rootPath="$4"       ## "%R"
+savePath="$5"       ## "%D"
+files="$6"          ## "%C"
+size="$7"           ## "%Z"
+tracker="$8"        ## "%T"
+torrentHash="$9"    ## "%I"
 ## Todo: add tag support when implemented in qBittorrent
 
 ## CSV format for logfile
 ## Date, Name, Hash, Category, Size, Imported(1/0)
 
-output="$(date +%c), '$name', '$hash', '$category', '$size',"
+output="$(date +%c), '$name', '$torrentHash', '$category', '$size',"
 
 if [[ "$category" == "do not import" ]]; then
     copy=false
@@ -36,7 +36,7 @@ fi
 output+=copy
 
 if [ $copy == true ]; then
-    cp "$cpath/" "$importFolder/" -r
+    cp "$contentPath/" "$importFolder/" -r
 fi
 
-echo output >> $logfile
+echo output >> $logFile
