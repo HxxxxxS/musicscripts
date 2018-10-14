b651a13547c31632ee63d4bdd2fb632c9938d3e6
sophusweidemann <sophusaugustweidemann@gmail.com>
2018-10-14 17:22:50 +0200

    Move public torrents, plus series and videos

--- copy_from_qBittorrent.sh
+++ copy_from_qBittorrent.sh
@@ -14,29 +14,75 @@ files="$6"          ## "%C"
 size="$7"           ## "%Z"
 tracker="$8"        ## "%T"
 torrentHash="$9"    ## "%I"
-## Todo: add tag support when implemented in qBittorrent
+tags="$10"          ## "%G"
 
 ## CSV format for logfile
-## Date, Name, Hash, Category, Size, Imported(true/false)
+## Date, Name, Hash, Category, Size, Tags
 
 output="$(date +%c), '$name', $torrentHash, '$category', $size, "
 
 if [[ "$category" == "do not import" ]]; then
-    copy=false
+    music=false
 elif [[ "$category" == "old"* ]]; then
-    copy=false
+    music=false
+elif [[ "$category" == "redacted" ]]; then
+    music=true
 elif [[ "$category" == "musikk" ]]; then
-    copy=true
+    music=true
+elif [[ "$tracker" == *"flacsfor.me"* ]]; then
+    music=true
 elif [[ "$tracker" == *"apollo"* ]]; then
-    copy=true
+    music=true
 else
-    copy=false
+    music=false
 fi
 
-output+=$copy
+if [[ "$category" == "serier" ]]; then
+    series=true
+elif [[ "$category" == "film" ]]; then
+    film=true
+fi
 
-if [ $copy == true ]; then
-    cp "$contentPath/" "$importFolder/" -r
+public=false
+
+if [[ "$tags" == *"public"* ]]; then
+    public=true
+elif [[ "$tracker" == *"t-ru.org"* ]]; then
+    public=true
+elif [[ "$tracker" == *"openbittorrent.com"* ]]
+    public=true
+elif [[ "$tracker" == *"rarbg.com"* ]]
+    public=true
+elif [[ "$tracker" == *"zer0day.to"* ]]
+    public=true
+elif [[ "$tracker" == *"leechers-paradise.org"* ]]
+    public=true
+elif [[ "$tracker" == *"coppersurfer.tk"* ]]
+    public=true
+elif [[ "$tracker" == *"1337x.org"* ]]
+    public=true
+elif [[ "$tracker" == *"piratebay"* ]]
+    public=true
+fi
+
+output+=$tags
+
+if [[ $public ]]; then
+    if [ $music ]; then
+        mv "$contentPath/" "$importFolder/"
+    elif [ $series ]; then
+        mv "$contentPath/" "~/Series/"
+    elif [[ $film ]]; then
+        mv "$contentPath/" "~/Videos/"
+    fi
+else
+    if [ $music ]; then
+        cp "$contentPath/" "$importFolder/" -r
+    elif [ $series ]; then
+        cp "$contentPath/" "~/Series/" -r
+    elif [[ $film ]]; then
+        cp "$contentPath/" "~/Videos/" -r
+    fi
 fi
 
 if [ ! -e "$logFile" ] ; then
